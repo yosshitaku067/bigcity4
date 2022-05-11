812c8d8b5a1fd2b93417d08bf4268e2293fdb1a4
yoshida takuya <y.takuya77@gmail.com>
2022-05-11 16:14:16 +0900

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
-import * as React from 'react';
-import './style.css';
+import React from "react";
+import "./App.css";
 
-export default function App() {
-  return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
-    </div>
-  );
+function App() {
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
 }
+
+export default App;
