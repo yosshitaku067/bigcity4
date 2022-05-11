37519b1a0ef7f954efde3bf3c0e0767a96272570
yosshitaku067 <y.takuya77@gmail.com>
2022-05-11 16:08:15 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-ts-gdrgw8
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-ts-gdrgw8)
---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import './style.css';
+
+export default function App() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Hello StackBlitz!</h1>
+      <p>Start editing to see some magic happen :)</p>
+    </div>
+  );
+}
